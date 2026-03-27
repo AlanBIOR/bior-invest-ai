522c56e600a204fdcd1ed4a26a278e50954a2a7a
Alan Rodriguez <tu_email_de_github@ejemplo.com>
2026-03-26 23:23:49 -0600

    Agregando models

--- investments/models.py
+++ investments/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from decimal import Decimal
 
 # --- 1. CATEGORÍAS DE INVERSIÓN ---
 class Category(models.Model):
